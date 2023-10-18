c9aaab41924debbd843a09f2e19cedf5d52fff03
Alexis Ngaing <ksankirara92@gmail.com>
2023-10-18 09:00:00 +0700

    update

---
+++ lib/view/register.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:ugd_4_hospital/component/form_component';
+import 'package:intl/intl.dart';
+import 'package:ugd_4_hospital/view/login.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({Key? key, required Map data}) : super(key: key);
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController notelpController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              inputForm((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'Username tidak  boleh kosong';
+                }
+                if (p0.toLowerCase() == 'anjing') {
+                  return 'tidak boleh menggunakan kata kasar';
+                }
+                return null;
+              },
+                  controller: usernameController,
+                  hintTxt: "Username",
+                  helperTxt: "Ucup Surucup",
+                  iconData: Icons.person),
+              inputForm(((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'Email tidak boleh kosong';
+                }
+                if (!p0.contains('@')) {
+                  return 'Email harus menggunakan @';
+                }
+                return null;
+              }),
+                  controller: emailController,
+                  hintTxt: "Email",
+                  helperTxt: "ucup@gmail.com",
+                  iconData: Icons.email),
+              inputForm(((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'password tidak boleh kosong';
+                }
+                if (p0.length < 5) {
+                  return 'Password minimal 5 digit';
+                }
+                return null;
+              }),
+                  controller: passwordController,
+                  hintTxt: "Password",
+                  helperTxt: "xxxxxxx",
+                  iconData: Icons.password,
+                  password: true),
+              inputForm(((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'Nomor Telepon tidak boleh kosong';
+                }
+                return null;
+              }),
+                  controller: notelpController,
+                  hintTxt: "No Telp",
+                  helperTxt: "082123456789",
+                  iconData: Icons.phone_android),
+              Container(
+                width: 370,
+                padding: const EdgeInsets.only(left: 20, top: 10),
+                child: TextFormField(
+                  controller: dateController,
+                  decoration: InputDecoration(
+                    hintText: 'Tanggal Lahir',
+                    helperText: 'Format: DD/MM/YYYY',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.calendar_today),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Tanggal Lahir tidak boleh kosong';
+                    }
+
+                    return null;
+                  },
+                  onTap: () async {
+                    DateTime? pickerDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1950),
+                      lastDate: DateTime.now(),
+                    );
+                    if (pickerDate != null) {
+                      dateController.text =
+                          DateFormat('yyyy/MM/dd').format(pickerDate);
+                    }
+                  },
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Map<String, dynamic> formData = {};
+                    formData['username'] = usernameController.text;
+                    formData['password'] = passwordController.text;
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => LoginPage(
+                          data: formData,
+                        ),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Register'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
