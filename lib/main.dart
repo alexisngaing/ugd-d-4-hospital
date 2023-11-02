c8a0f2fb675afd3a46f143e76463d0ad2b29b25d
2yool <zidaneir01@gmail.com>
2023-11-02 15:37:20 +0700

    sisa method untuk camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ugd_4_hospital/View/login.dart';
+import 'package:ugd_4_hospital/View/home.dart';
 // import 'package:ugd_4_hospital/view/login.dart';
 
 void main() {
