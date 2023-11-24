44efb03e0f4d0aaf6fcae7a257854d8f9ec4552f
AxelChristiann <axelchristiann261@gmail.com>
2023-11-24 15:52:29 +0700

    Update profil, kurang bagian save image

--- lib/View/profile.dart
+++ lib/View/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ugd_4_hospital/data/User.dart';
+import 'package:ugd_4_hospital/database/API/UserClient.dart';
 import 'package:ugd_4_hospital/database/sql_helper_profile.dart';
 import 'package:ugd_4_hospital/utils/toast_util.dart';
 import 'package:ugd_4_hospital/View/home.dart';
@@ -18,7 +20,10 @@ class _ProfileState extends State<Profile> {
   late TextEditingController emailController;
   late TextEditingController passwordController;
   late TextEditingController noTelpController;
+  int? idUser;
   bool isPasswordVisible = false;
+  bool isLoading = false;
+  final _formKey = GlobalKey<FormState>();
   Key imageKey = UniqueKey();
   Uint8List? imageFile;
   final imagePicker = ImagePicker();
@@ -36,148 +41,155 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 50, left: 20, right: 20),
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: ListView(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  showPictureDialog();
-                },
-                child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage:
-                        imageFile != null ? MemoryImage(imageFile!) : null,
-                    child: const Align(
-                      alignment: Alignment.bottomRight,
-                      child: CircleAvatar(
-                        radius: 15,
-                        backgroundColor: Colors.blue,
-                        child: Icon(
-                          Icons.edit,
-                          color: Colors.white,
-                          size: 18,
+      body: Center(
+        child: isLoading
+            ? CircularProgressIndicator()
+            : Container(
+                padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+                child: GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: ListView(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          showPictureDialog();
+                        },
+                        child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: imageFile != null
+                                ? MemoryImage(imageFile!)
+                                : null,
+                            child: const Align(
+                              alignment: Alignment.bottomRight,
+                              child: CircleAvatar(
+                                radius: 15,
+                                backgroundColor: Colors.blue,
+                                child: Icon(
+                                  Icons.edit,
+                                  color: Colors.white,
+                                  size: 18,
+                                ),
+                              ),
+                            )),
+                      ),
+                      Center(
+                        child: Text(
+                          "Dodi FirmanSyahhhhhhh",
+                          style: TextStyle(
+                            fontSize: 16.sp,
+                            color: Colors.black.withOpacity(0.7),
+                          ),
                         ),
                       ),
-                    )),
-              ),
-              Center(
-                child: Text(
-                  "Dodi FirmanSyahhhhhhh",
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    color: Colors.black.withOpacity(0.7),
+                      TextField(
+                        controller: usernameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Username',
+                          prefixIcon: Icon(Icons.person),
+                        ),
+                      ),
+                      TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          prefixIcon: Icon(Icons.email),
+                        ),
+                        enabled: false,
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.lock),
+                            labelText: 'Password',
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isPasswordVisible = !isPasswordVisible;
+                                });
+                              },
+                              icon: Icon(
+                                isPasswordVisible
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: isPasswordVisible
+                                    ? Colors.grey
+                                    : Colors.blue,
+                              ),
+                            )),
+                        obscureText: isPasswordVisible,
+                      ),
+                      TextField(
+                        controller: noTelpController,
+                        decoration: const InputDecoration(
+                          labelText: 'No. Telp',
+                          prefixIcon: Icon(Icons.numbers),
+                        ),
+                      ),
+                      SizedBox(height: 3.h),
+                      SizedBox(height: 3.h),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              loadUserData();
+                              _updateUserData();
+                              showToast('Berhasil Ubah Data');
+                              _reloadProfile();
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => const HomePage(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "SAVE",
+                              style: TextStyle(
+                                fontSize: 15.sp,
+                                letterSpacing: 2,
+                                color: Colors.white,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              padding: EdgeInsets.symmetric(horizontal: 5.h),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              loadUserData();
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => const HomePage(),
+                                ),
+                              );
+                            },
+                            child: Text("CANCEL",
+                                style: TextStyle(
+                                  fontSize: 15.sp,
+                                  letterSpacing: 2,
+                                  color: Colors.black,
+                                )),
+                            style: OutlinedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(horizontal: 5.h),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
-              TextField(
-                controller: usernameController,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  prefixIcon: Icon(Icons.person),
-                ),
-              ),
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  prefixIcon: Icon(Icons.email),
-                ),
-                enabled: false,
-              ),
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.lock),
-                    labelText: 'Password',
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          isPasswordVisible = !isPasswordVisible;
-                        });
-                      },
-                      icon: Icon(
-                        isPasswordVisible
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                        color: isPasswordVisible ? Colors.grey : Colors.blue,
-                      ),
-                    )),
-                obscureText: isPasswordVisible,
-              ),
-              TextField(
-                controller: noTelpController,
-                decoration: const InputDecoration(
-                  labelText: 'No. Telp',
-                  prefixIcon: Icon(Icons.numbers),
-                ),
-              ),
-              SizedBox(height: 3.h),
-              SizedBox(height: 3.h),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      loadUserData();
-                      _updateUserData();
-                      showToast('Berhasil Ubah Data');
-                      _reloadProfile();
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => const HomePage(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      "SAVE",
-                      style: TextStyle(
-                        fontSize: 15.sp,
-                        letterSpacing: 2,
-                        color: Colors.white,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      padding: EdgeInsets.symmetric(horizontal: 5.h),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                  ),
-                  OutlinedButton(
-                    onPressed: () {
-                      loadUserData();
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => const HomePage(),
-                        ),
-                      );
-                    },
-                    child: Text("CANCEL",
-                        style: TextStyle(
-                          fontSize: 15.sp,
-                          letterSpacing: 2,
-                          color: Colors.black,
-                        )),
-                    style: OutlinedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(horizontal: 5.h),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
@@ -206,34 +218,59 @@ class _ProfileState extends State<Profile> {
     );
   }
 
+  void showSnackBar(BuildContext context, String msg, Color bg) {
+    final Scaffold = ScaffoldMessenger.of(context);
+    Scaffold.showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        backgroundColor: bg,
+        action: SnackBarAction(
+            label: 'hide', onPressed: Scaffold.hideCurrentSnackBar),
+      ),
+    );
+  }
+
   Future<void> loadUserData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    idUser = prefs.getInt('id_user');
     setState(() {
-      emailController.text = prefs.getString('email') ?? '';
+      isLoading = true;
     });
-    final userData = await _getUserData();
-    if (userData != null) {
-      usernameController.text = userData['username'] ?? '';
-      emailController.text = userData['email'] ?? '';
-      passwordController.text = userData['password'] ?? '';
-      noTelpController.text = userData['noTelp'] ?? '';
-      imageFile = userData['foto'] ?? '';
+    try {
+      User res = await UserClient.find(idUser);
+      setState(() {
+        isLoading = false;
+        usernameController.value = TextEditingValue(text: res.username);
+        emailController.value = TextEditingValue(text: res.email);
+        passwordController.value = TextEditingValue(text: res.password);
+        noTelpController.value = TextEditingValue(text: res.noTelp);
+      });
+    } catch (err) {
+      showSnackBar(context, err.toString(), Colors.red);
+      Navigator.pop(context);
     }
   }
 
-  Future<Map<String, dynamic>?> _getUserData() async {
-    return await SQLHelperProfile.getUserByEmail(emailController.text);
-  }
-
   Future<void> _updateUserData() async {
-    String updatedUsername = usernameController.text;
-    String updatedEmail = emailController.text;
-    String updatedPassword = passwordController.text;
-    String updatedNoTelp = noTelpController.text;
+    if (!_formKey.currentState!.validate()) return;
     Uint8List? foto = imageFile;
+    User input = User(
+      id: idUser!,
+      username: usernameController.text,
+      email: emailController.text,
+      password: passwordController.text,
+      noTelp: noTelpController.text,
+    );
 
-    await SQLHelperProfile.editUserByUsername(
-        updatedUsername, updatedEmail, updatedPassword, updatedNoTelp, foto!);
+    try {
+      await UserClient.update(input);
+
+      showSnackBar(context, 'Success', Colors.green);
+      Navigator.pop(context);
+    } catch (err) {
+      showSnackBar(context, err.toString(), Colors.red);
+      Navigator.pop(context);
+    }
   }
 
   Future<void> showPictureDialog() async {
